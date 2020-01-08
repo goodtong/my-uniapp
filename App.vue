2326536b752a0f9ca796f89b0be83a678c8eff42
liuxiaotong <987717609@qq.com>
2020-01-08 18:30:00 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.log('App Launch')
+	},
+	onShow: function() {
+		console.log('App Show')
+	},
+	onHide: function() {
+		console.log('App Hide')
+	},
+	methods: {
+		/**
+		 * 处理推送消息
+		 */
+		handlePush() {
+			// #ifdef APP-PLUS
+			const _self = this
+			const _handlePush = function(message) {
+				// 获取自定义信息
+				// let payload = message.payload
+				// try {
+				// 	// JSON解析
+				// 	payload = JSON.parse(payload)
+				// 	// 携带自定义信息跳转应用页面
+				// 	uni.navigateTo({
+				// 		url: '/pages/xxx?data=' + JSON.stringify(payload)
+				// 	})
+				// } catch (e) {}
+				plus.runtime.setBadgeNumber(1)
+				console.log(message)
+			}
+			// 事件处理
+			plus.push.addEventListener('click', _handlePush)
+			plus.push.addEventListener('receive', _handlePush)
+			// #endif
+		}
+	}
+}
+</script>
+
+<style>
+/*每个页面公共css */
+/* 引入官方css库 */
+@import './common/uni.css';
+/* 引入自定义图标库 */
+@import './common/icon.css';
+/* 引入动画库 */
+@import './common/animate.css';
+/* 公共样式 */
+@import './common/common.css';
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,95 @@
+<template>
+	<view>
+		<view class="index-list" v-for="(item, index) in 10" :key="index">
+			<view class="index-list1 u-f-ac u-f-jsb">
+				<view class="u-f-ac">
+					<image src="../../static/demo/userpic/14.jpg" mode="widthFix" lazy-load></image>
+					昵称
+				</view>
+				<view class="u-f-ac">
+					<view class="icon iconfont icon-zengjia"></view>
+					关注
+				</view>
+			</view>
+			<view class="index-list2">我是标题</view>
+			<view class="index-list3 u-f-ajc">
+				<image src="../../static/demo/datapic/29.jpg" mode="widthFix" lazy-load></image>
+			</view>
+			<view class="index-list4 u-f-ac u-f-jsb">
+				<view class="u-f-ac">
+					<view class="u-f-ac">
+						<view class="icon iconfont icon-icon_xiaolian-mian"></view>
+						987
+					</view>
+					<view class="u-f-ac">
+						<view class="icon iconfont icon-kulian"></view>
+						10
+					</view>
+				</view>
+				<view class="u-f-ac">
+					<view class="u-f-ac">
+						<view class="icon iconfont icon-pinglun1"></view>
+						34
+					</view>
+					<view class="u-f-ac">
+						<view class="icon iconfont icon-zhuanfa"></view>
+						7
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {}
+	},
+	onLoad() {},
+	methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.index-list {
+	padding: 20upx;
+	border-bottom: 1upx solid #eeeeee;
+}
+.index-list1 > view:first-child {
+	color: #999999;
+}
+.index-list1 > view:first-child image {
+	width: 85upx;
+	height: 85upx;
+	border-radius: 100%;
+	margin-right: 15upx;
+}
+.index-list1 > view:last-child {
+	background: #f4f4f4;
+	border-radius: 5upx;
+	padding: 0 10upx;
+}
+.index-list2 {
+	padding-top: 15upx;
+	font-size: 32upx;
+}
+.index-list3 {
+	// position: relative;
+	padding-top: 15upx;
+}
+.index-list3 > image {
+	width: 100%;
+	border-radius: 20upx;
+}
+.index-list4 view {
+	color: #d5d5d5;
+}
+.index-list4 {
+	padding: 15upx 0;
+}
+.index-list4 > view > view > view,
+.index-list4 > view > view:first-child {
+	margin-right: 15upx;
+}
+</style>
